52fb6cc185b3e19db1016b5e254b3699d82dd0dc
Wan Muzaffar <wanmuz86@gmail.com>
2025-02-16 15:53:41 +0800

    add padding

--- app/src/main/java/com/muzaffar/apiexample/MainActivity.kt
+++ app/src/main/java/com/muzaffar/apiexample/MainActivity.kt
@@ -17,8 +17,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
@@ -33,12 +35,22 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             APIExampleTheme {
-                PostListScreen()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(title = { Text(text = "API Example") })
+                    }
+                ) {
+                    padding->
+                    PostListScreen(modifier=Modifier.padding(padding))
+                }
+
             }
         }
     }
@@ -61,7 +73,7 @@ fun PostItem(post:Post){
 }
 
 @Composable
-fun PostListScreen(viewModel: PostViewModel = viewModel()){
+fun PostListScreen(modifier:Modifier, viewModel: PostViewModel = viewModel()){
     val posts by viewModel.posts.observeAsState(emptyList())
     val isLoading by viewModel.isLoading
     val error by viewModel.error
@@ -71,7 +83,7 @@ fun PostListScreen(viewModel: PostViewModel = viewModel()){
         // Call the API to fetch the posts
         viewModel.fetchPosts() //If there is a change in the ViewModel, the composable will recompose
     }
-    Box(modifier= Modifier.fillMaxSize()){
+    Box(modifier= modifier.fillMaxSize().padding()){
             when {
                 // If the page is loading, show circular progress indicator
                 isLoading -> {
